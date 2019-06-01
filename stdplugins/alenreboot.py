541c1544c141102e7f80fd6f04c0527f3ee55705
AlenPaulVarghese <38587581+alenpaul2001@users.noreply.github.com>
2019-06-01 16:37:08 +0530

    Create alenreboot.py

---
+++ stdplugins/alenreboot.py
@@ -0,0 +1,18 @@
+# (c) @UniBorg
+# Original written by @UniBorg edit by @INF1N17Y
+
+from telethon import events
+import asyncio
+from collections import deque
+
+
+@borg.on(events.NewMessage(pattern=r"\.alen", outgoing=True))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("ALEN"))
+	for _ in range(21):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
+    
