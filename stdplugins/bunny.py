3107564b9cc3022c1e64228ee7aff74e35b52b8f
AlenPaulVarghese <38587581+alenpaul2001@users.noreply.github.com>
2019-06-06 16:35:55 +0530

    Create bunny.py

---
+++ stdplugins/bunny.py
@@ -0,0 +1,16 @@
+# BY @STARKTM1
+from telethon import events
+import asyncio
+import os
+import sys
+
+
+@borg.on(events.NewMessage(pattern=r"\.Acad", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+       
+
+    await event.edit("(\_(\ \
+                     (=' :') ~♥ \
+                     (,(')(') ")
