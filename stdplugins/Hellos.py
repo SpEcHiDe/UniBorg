1b242edad2b02db74214f2a70dd093d9b64d3910
AlenPaulVarghese <38587581+alenpaul2001@users.noreply.github.com>
2019-06-07 15:24:10 +0530

    Create Hellos.py

---
+++ stdplugins/Hellos.py
@@ -0,0 +1,14 @@
+# By STARKTM1
+from telethon import events
+import asyncio
+import os
+import sys
+
+
+@client.on(events.NewMessage(outgoing=True, pattern=r'\.save'))
+async def handler(event):
+    if event.is_reply:
+        replied = await event.get_reply_message()
+        sender = replied.sender
+        await client.download_profile_photo(sender)
+        await event.respond('Saved your photo {}'.format(sender.username))
